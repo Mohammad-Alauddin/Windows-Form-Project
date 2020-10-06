4ed97eb4f72df6201282b048b05e4dc7cd6e5203
Md Alauddin <alauddinahsan@gmail.com>
2020-10-06 12:35:30 +0600

    First Commit

---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtStoreName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtstate = new System.Windows.Forms.TextBox();
+            this.txtZip_code = new System.Windows.Forms.TextBox();
+            this.txtstreet = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 9);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1073, 223);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // txtStoreName
+            // 
+            this.txtStoreName.Location = new System.Drawing.Point(29, 276);
+            this.txtStoreName.Name = "txtStoreName";
+            this.txtStoreName.Size = new System.Drawing.Size(100, 20);
+            this.txtStoreName.TabIndex = 1;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(135, 276);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 20);
+            this.txtPhone.TabIndex = 2;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(453, 276);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(241, 276);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // txtstate
+            // 
+            this.txtstate.Location = new System.Drawing.Point(559, 276);
+            this.txtstate.Name = "txtstate";
+            this.txtstate.Size = new System.Drawing.Size(100, 20);
+            this.txtstate.TabIndex = 7;
+            // 
+            // txtZip_code
+            // 
+            this.txtZip_code.Location = new System.Drawing.Point(683, 276);
+            this.txtZip_code.Name = "txtZip_code";
+            this.txtZip_code.Size = new System.Drawing.Size(100, 20);
+            this.txtZip_code.TabIndex = 6;
+            // 
+            // txtstreet
+            // 
+            this.txtstreet.Location = new System.Drawing.Point(347, 276);
+            this.txtstreet.Name = "txtstreet";
+            this.txtstreet.Size = new System.Drawing.Size(100, 20);
+            this.txtstreet.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(801, 238);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(223, 155);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "store_name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "phone";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(278, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(375, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "street";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(485, 238);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "city";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(588, 238);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "state";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(711, 238);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "zip_code";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Save Or Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(453, 349);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1087, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtstate);
+            this.Controls.Add(this.txtZip_code);
+            this.Controls.Add(this.txtstreet);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtStoreName);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtStoreName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtstate;
+        private System.Windows.Forms.TextBox txtZip_code;
+        private System.Windows.Forms.TextBox txtstreet;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        int rowNum = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            fillDrid();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sqlString = "";
+            if (rowNum == 0)
+            {
+
+
+                sqlString = @"INSERT INTO [dbo].[Fornt_Store]
+           ([store_name]
+           ,[phone]
+           ,[email]
+           ,[street]
+           ,[city]
+           ,[state]
+           ,[zip_code]
+           ,[photo])
+            VALUES
+           (@store_name
+           ,@phone
+           ,@email
+           ,@street
+           ,@city
+           ,@state
+           ,@zip_code
+           ,@photo)";
+            }
+            else
+            {
+
+                sqlString = @"UPDATE [dbo].[Fornt_Store]
+   SET [store_name] = @store_name
+      ,[phone] = @phone 
+      ,[email] = @email
+      ,[street] = @street
+      ,[city] = @city
+      ,[state] = @state
+      ,[zip_code] = @zip_code
+      ,[photo] = @photo
+ WHERE  store_id =@store_id ";
+
+
+            }
+
+
+
+            using (OpenFileDialog ofd = new OpenFileDialog() { })
+            {
+                ofd.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    SqlConnection scon = new SqlConnection(ConfigurationManager.ConnectionStrings[0].ConnectionString);
+                    scon.Open();
+                    SqlCommand cmd = new SqlCommand(sqlString, scon);
+                    cmd.CommandType = CommandType.Text;
+                   
+                    cmd.Parameters.Add("@store_name", SqlDbType.VarChar).Value = txtStoreName.Text;
+                    cmd.Parameters.Add("@phone", SqlDbType.VarChar).Value = txtPhone.Text;
+                    cmd.Parameters.Add("@email", SqlDbType.VarChar).Value = txtEmail.Text;
+                    cmd.Parameters.Add("@street", SqlDbType.VarChar).Value = txtstreet.Text;
+                    cmd.Parameters.Add("@city", SqlDbType.VarChar).Value = txtCity.Text;
+                    cmd.Parameters.Add("@state", SqlDbType.VarChar).Value = txtstate.Text;
+                    cmd.Parameters.Add("@zip_code", SqlDbType.VarChar).Value = txtZip_code.Text;
+                    if(rowNum != 0)
+                    {
+                        cmd.Parameters.Add("@store_id", SqlDbType.Int).Value = rowNum;
+                    }
+                   
+
+                    pictureBox1.Image = new Bitmap(ofd.FileName);
+                    ImageConverter ic = new ImageConverter();
+                    byte[] imagebyte = (byte[])ic.ConvertTo(pictureBox1.Image, typeof(byte[]));
+                    cmd.Parameters.Add("@photo", SqlDbType.VarBinary).Value = imagebyte;
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(ds);
+
+                    fillDrid();
+
+                    rowNum = 0;
+
+
+                    //DataGridViewImageColumn icCol = new DataGridViewImageColumn();
+                    //icCol.HeaderText = "Img";
+                    //icCol.Image = null;
+                    //icCol.Name = "cImg";
+                    //icCol.Width = 100;
+                    //dataGridView1.Columns.Add(icCol);
+
+                    //foreach (DataGridViewRow row in dataGridView1.Rows)
+                    //{
+                    //    try
+                    //    {
+                    //        DataGridViewImageCell cell = row.Cells[row.Cells.Count - 2] as DataGridViewImageCell;
+                    //        cell.Value = getImageFromByte(row);
+                    //    }catch(Exception ex)
+                    //    {
+
+                    //    }
+                    //}
+
+                }
+
+            }
+
+
+        }
+
+        private void fillDrid()
+        {
+            SqlConnection scon1 = new SqlConnection(ConfigurationManager.ConnectionStrings[0].ConnectionString);
+            scon1.Open();
+            SqlCommand cmd1 = new SqlCommand(@"SELECT [store_id]
+                   ,[store_name]
+                   ,[phone]
+                  ,[email]
+                   ,[street]
+                   ,[city]
+                  ,[state]
+                  ,[zip_code]
+                   ,[photo]
+                   FROM [dbo].[Fornt_Store]", scon1);
+            cmd1.CommandType = CommandType.Text;
+
+            SqlDataAdapter sda1 = new SqlDataAdapter(cmd1);
+            ds.Clear();
+            sda1.Fill(ds);
+
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        //private Image getImageFromByte(DataGridViewRow dr)
+        //{
+
+
+        //    var byteArray =  (byte[])dr.Cells["photo"].Value;
+        //    MemoryStream ms = new MemoryStream(byteArray);
+        //    return Image.FromStream(ms);
+
+        //}
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            DataTable dt = (DataTable)dataGridView1.DataSource;
+            try
+            {
+                rowNum = Convert.ToInt32(dt.Rows[e.RowIndex]["store_id"].ToString());
+            }
+            catch (Exception ex)
+            {
+                rowNum = 0;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sqlString = "";
+            if (rowNum != 0)
+            {
+                sqlString = @"delete from  [dbo].[Fornt_Store] where store_id=" + rowNum.ToString();
+
+                SqlConnection scon1 = new SqlConnection(ConfigurationManager.ConnectionStrings[0].ConnectionString);
+                scon1.Open();
+                SqlCommand cmd1 = new SqlCommand(sqlString, scon1);
+                cmd1.CommandType = CommandType.Text;
+
+                SqlDataAdapter sda1 = new SqlDataAdapter(cmd1);
+                ds.Clear();
+                sda1.Fill(ds);
+
+                fillDrid();
+                rowNum = 0;
+
+            }
+        }
+    }
+}
